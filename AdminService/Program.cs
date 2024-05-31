0d83b62cc4fa1573d984d51a71de57562b97432b
Trần Quang Huy <huy.com2003@gmail.com>
2024-05-31 15:33:18 +0700

    Add Logging for admin services

--- AdminService/Program.cs
+++ AdminService/Program.cs
@@ -1,4 +1,6 @@
-using AdminService.Entities;
+﻿using AdminService.Entities;
+using Serilog;
+using Serilog.Formatting.Json;
 using ServicesCommon.MassTransit;
 using ServicesCommon.MongoDB;
 
@@ -11,6 +13,16 @@ builder.Services.AddControllers();
 builder.Services.AddMongo()
     .AddMongoRepository<Admin>("Admins_")
     .AddMassTransitWithRabbitMq();
+
+builder.Host.UseSerilog((ctx, config) =>
+{
+    config.WriteTo.Console().MinimumLevel.Information();
+    config.WriteTo.File(
+        path: "D:\\Bài làm các môn\\Mẫu thiết kế\\BackEnd\\Logs\\Admin\\AdminLog-.txt",
+        rollingInterval: RollingInterval.Day,
+        rollOnFileSizeLimit: true,
+        formatter: new JsonFormatter()).MinimumLevel.Information();
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -25,6 +37,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.MapHealthChecks("health");
+
+app.UseSerilogRequestLogging();
 
 app.UseHttpsRedirection();
 
