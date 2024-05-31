b780f6e2b4c29413d4460ce7d4c4e3fad7ef399e
Trần Quang Huy <huy.com2003@gmail.com>
2024-05-31 15:40:09 +0700

    Add Logging for user services

--- User/Program.cs
+++ User/Program.cs
@@ -1,4 +1,6 @@
-using MassTransit;
+﻿using MassTransit;
+using Serilog;
+using Serilog.Formatting.Json;
 using ServicesCommon.MassTransit;
 using ServicesCommon.MongoDB;
 using User.Entities;
@@ -24,6 +26,17 @@ builder.Services.AddMongo()
     .AddMongoRepository<Users>("Users")
     .AddMassTransitWithRabbitMq();
 
+builder.Host.UseSerilog((ctx, config) =>
+{
+    config.WriteTo.Console().MinimumLevel.Information();
+    config.WriteTo.File(
+        path: "D:\\Bài làm các môn\\Mẫu thiết kế\\BackEnd\\Logs\\User\\UserLog-.txt",
+        rollingInterval: RollingInterval.Day,
+        rollOnFileSizeLimit: true,
+        formatter: new JsonFormatter()).MinimumLevel.Information();
+});
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -37,6 +50,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 app.MapHealthChecks("health");
+
+app.UseSerilogRequestLogging();
 
 app.UseHttpsRedirection();
 
